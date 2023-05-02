cb983b0322bba3bd48b0d70001722c6804ea5aed
Salomėja <87271809+salomeja2000@users.noreply.github.com>
2023-05-02 15:39:47 +0300

    Time: 1 ms, Memory: 40.5 MB - LeetHub

---
+++ implement-strstr/implement-strstr.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            if (haystack.charAt(i) == needle.charAt(0)) {
+                boolean match = true;
+                for (int j = 1; j < needle.length(); j++) {
+                    if (haystack.charAt(i+j) != needle.charAt(j)) {
+                        match = false;
+                        break;
+                    }
+                }
+                if (match) return i;
+            }
+        }
+        return -1; 
+    }
+}
